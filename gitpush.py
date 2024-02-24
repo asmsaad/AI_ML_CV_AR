64fa531a77a658e21e5120f566ac0f23398872c4
asmsaad <shadmia6@gmail.com>
2024-02-24 20:30:25 +0600

    first commit22

--- gitpush.py
+++ gitpush.py
@@ -8,11 +8,8 @@ from datetime import datetime
 
 # Get current date and time
 current_datetime = datetime.now()
-
-# Format date and time
 formatted_datetime = current_datetime.strftime("%d%b%Y_%I:%M%p")
 
-print(formatted_datetime)
 
 
 def header(header):
@@ -29,6 +26,8 @@ header('Adding Changes')
 os.system("git add . ")
 header('Added Status')
 os.system("git status")
-header('UPLOADING...')
+header('Uploading...')
 os.system('git commit -m "first commit22" ')
 os.system("git push -u origin main")
+print(' ' +  formatted_datetime.center(51,' ') + ' ') 
+print()
