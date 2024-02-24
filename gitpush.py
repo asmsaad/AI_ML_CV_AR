615e58a093b55e00d8c8328878061b63dd6d6842
asmsaad <shadmia6@gmail.com>
2024-02-24 20:11:11 +0600

    first commit22

---
+++ gitpush.py
@@ -0,0 +1,12 @@
+
+
+ 
+ 
+ 
+import os
+ 
+os.system("git status")
+os.system("git add . ")
+os.system("git status")
+os.system('git commit -m "first commit22" ')
+os.system("git push -u origin main")
