0896a93207524a4e6a8e90f11be75698ef78ad4b
asmsaad <shadmia6@gmail.com>
2024-02-24 20:15:01 +0600

    first commit22

--- gitpush.py
+++ gitpush.py
@@ -4,9 +4,20 @@
  
  
 import os
- 
+
+def header():
+    print('┌────────────────────────────────────┐') 
+    print('│                                    │') 
+    print('└────────────────────────────────────┘') 
+
+
+
+header()
 os.system("git status")
+header()
 os.system("git add . ")
+header()
 os.system("git status")
+header()
 os.system('git commit -m "first commit22" ')
 os.system("git push -u origin main")
